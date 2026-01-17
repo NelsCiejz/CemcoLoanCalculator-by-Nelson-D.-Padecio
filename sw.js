fe7a46201a9786b068658f72412dde0e0fc726ad
NelsCiejz <nelsciejz725@yahoo.com>
2026-01-17 14:37:55 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,32 @@
+const CACHE_NAME = 'cemco-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icon.png'
+];
+
+// Install Service Worker and Cache Files
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate and Clean Up Old Caches
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker Activated');
+});
+
+// Fetch Request
 self.addEventListener('fetch', (event) => {
-  // Kahit walang laman na cache, kailangan ito para maging "Installable"
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
